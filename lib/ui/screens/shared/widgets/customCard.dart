c21e1c803ced037702055fd9d7f155ecc56de09c
jrmpcnd <jeremae.apacionado@cardmri.com>
2025-05-27 21:20:43 +0800

    custom card updated

--- lib/ui/screens/shared/widgets/customCard.dart
+++ lib/ui/screens/shared/widgets/customCard.dart
@@ -10,10 +10,12 @@ class CustomCardWidget extends StatelessWidget {
   final List<String>? buttonLabels;
   final VoidCallback? onMainActionPressed;
   final List<VoidCallback>? onButtonPressed;
+  final double? maxWidth;
 
   const CustomCardWidget({
     Key? key,
     this.title,
+    this.maxWidth,
     required this.description,
     required this.imagePath,
     this.buttonLabels,
@@ -95,6 +97,149 @@ class CustomCardWidget extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         if (title != null)
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Text(''),
+              ),
+              Text(
+                title!,
+                style: TextStyle(
+                  fontSize: titleFontSize,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        const SizedBox(height: 10),
+        Text(
+          description,
+          style: TextStyle(fontSize: descFontSize),
+          textAlign: TextAlign.justify,
+        ),
+        const SizedBox(height: 20),
+        if (buttonLabels != null && buttonLabels!.isNotEmpty)
+          Wrap(
+            spacing: 10,
+            runSpacing: 10,
+            children: List.generate(buttonLabels!.length, (index) {
+              return Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.2), // Change this to your desired button color
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Text(
+                  buttonLabels![index],
+                  style: const TextStyle(
+                    color: Colors.black, // Text color
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              );
+            }),
+          )
+
+      ],
+    );
+  }
+}
+
+
+class CustomCardWidgetv2 extends StatelessWidget {
+  final String? title;
+  final String description;
+  final String imagePath;
+  final List<String>? buttonLabels;
+  final VoidCallback? onMainActionPressed;
+  final List<VoidCallback>? onButtonPressed;
+  final double? maxWidth;
+
+  const CustomCardWidgetv2({
+    Key? key,
+    this.title,
+    this.maxWidth,
+    required this.description,
+    required this.imagePath,
+    this.buttonLabels,
+    this.onMainActionPressed,
+    this.onButtonPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final isWide = constraints.maxWidth >= 600;
+
+        double titleFontSize = isWide ? 30 : 20;
+        double descFontSize = isWide ? 15 : 14;
+        double imageWidth = isWide ? 450 : constraints.maxWidth * 0.9;
+        double imageHeight = isWide ? 300 : 200;
+
+        Widget cardContent = Card(
+          surfaceTintColor: Colors.white,
+          shadowColor: Colors.grey,
+          margin: const EdgeInsets.all(10),
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40)),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Wrap(
+              alignment: isWide ? WrapAlignment.start : WrapAlignment.center,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              spacing: 40,
+              // space between items horizontally
+              runSpacing: 20,
+              // space between items vertically
+              children: [
+                Container(
+                  width: imageWidth,
+                  height: imageHeight,
+                  decoration: BoxDecoration(
+                    color: const Color(0xfff3f4f7),
+                    borderRadius: BorderRadius.circular(25),
+                    image: DecorationImage(
+                      image: AssetImage(imagePath),
+                      fit: BoxFit.contain, // better for various screen sizes
+                    ),
+                  ),
+                ),
+                ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minWidth: 200,
+                    maxWidth: isWide ? MediaQuery
+                        .of(context)
+                        .size
+                        .width * 0.6 : double.infinity,
+                  ),
+                  child: _buildTextSection(titleFontSize, descFontSize),
+                ),
+              ],
+            ),
+          ),
+        );
+
+        return onMainActionPressed != null
+            ? GestureDetector(
+          onTap: onMainActionPressed,
+          child: cardContent,
+        )
+            : cardContent;
+      },
+    );
+  }
+
+  Widget _buildTextSection(double titleFontSize, double descFontSize) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (title != null)
           Text(
             title!,
             style: TextStyle(
@@ -114,167 +259,25 @@ class CustomCardWidget extends StatelessWidget {
             spacing: 10,
             runSpacing: 10,
             children: List.generate(buttonLabels!.length, (index) {
-              return ElevatedButton(
-                onPressed: onButtonPressed != null && index < onButtonPressed!.length
-                    ? onButtonPressed![index]
-                    : () {},
-                child: Text(buttonLabels![index]),
+              return Container(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 16, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.2),
+                  // Change this to your desired button color
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Text(
+                  buttonLabels![index],
+                  style: const TextStyle(
+                    color: Colors.black, // Text color
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
               );
             }),
-          ),
-      ],
-    );
-  }
-}
-
-
-class CustomCardWidgetv2 extends StatelessWidget {
-  final String? title;
-  final String description;
-  final String imagePath;
-  final List<String>? buttonLabels;
-  final VoidCallback? onMainActionPressed;
-  final List<VoidCallback>? onButtonPressed;
-
-  const CustomCardWidgetv2({
-    Key? key,
-    this.title,
-    required this.description,
-    required this.imagePath,
-    this.buttonLabels,
-    this.onMainActionPressed,
-    this.onButtonPressed,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
-
-    // Determine layout sizes
-    double cardWidth = screenWidth >= 1024
-        ? 700
-        : screenWidth >= 600
-        ? screenWidth * 0.9
-        : screenWidth * 0.95;
-
-    double imageSize = screenWidth >= 1024
-        ? 100
-        : screenWidth >= 600
-        ? 80
-        : 60;
-
-    double textWidth = screenWidth >= 1024
-        ? 300
-        : screenWidth >= 600
-        ? 250
-        : double.infinity;
-
-    double titleFontSize = screenWidth >= 1024
-        ? 30
-        : screenWidth >= 600
-        ? 24
-        : 20;
-
-    double descriptionFontSize = screenWidth >= 600 ? 15 : 14;
-
-    Widget cardContent = Card(
-      color: Colors.white,
-      margin: const EdgeInsets.all(10),
-      elevation: 4,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-      child: Container(
-        width: cardWidth,
-        padding: const EdgeInsets.all(20),
-        child: screenWidth >= 600
-            ? Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Image
-            Container(
-              width: imageSize,
-              height: imageSize,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(imagePath),
-                  fit: BoxFit.cover,
-                ),
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-            const SizedBox(width: 30),
-            // Content
-            Expanded(
-              child: _buildTextSection(titleFontSize, descriptionFontSize),
-            ),
-          ],
-        )
-            : Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Image
-            Container(
-              width: imageSize,
-              height: imageSize,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(imagePath),
-                  fit: BoxFit.cover,
-                ),
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-            const SizedBox(height: 20),
-            _buildTextSection(titleFontSize, descriptionFontSize),
-          ],
-        ),
-      ),
-    );
-
-    return onMainActionPressed != null
-        ? GestureDetector(
-      onTap: onMainActionPressed,
-      child: cardContent,
-    )
-        : cardContent;
-  }
-
-  Widget _buildTextSection(double titleFontSize, double descFontSize) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        if (title != null)
-          Text(
-            title!,
-            style: TextStyle(
-              fontSize: titleFontSize,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        const SizedBox(height: 10),
-        ConstrainedBox(
-          constraints: BoxConstraints(
-            maxWidth: 700, // You can adjust this as needed
-          ),
-          child: Text(
-            description,
-            style: TextStyle(fontSize: descFontSize),
-            textAlign: TextAlign.justify,
-          ),
-        ),
-        const SizedBox(height: 20),
-        if (buttonLabels != null && buttonLabels!.isNotEmpty)
-          Wrap(
-            spacing: 10,
-            runSpacing: 10,
-            children: List.generate(buttonLabels!.length, (index) {
-              return ElevatedButton(
-                onPressed: onButtonPressed != null && index < onButtonPressed!.length
-                    ? onButtonPressed![index]
-                    : () {},
-                child: Text(buttonLabels![index]),
-              );
-            }),
-          ),
+          )
       ],
     );
   }
