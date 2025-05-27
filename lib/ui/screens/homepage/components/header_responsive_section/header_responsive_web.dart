b6beef072d79ff7d3c3e20ceec4835731b21fb80
jrmpcnd <jeremae.apacionado@cardmri.com>
2025-05-27 21:10:57 +0800

    responsive web the commit before this is the responsive mobile

--- lib/ui/screens/homepage/components/header_responsive_section/header_responsive_web.dart
+++ lib/ui/screens/homepage/components/header_responsive_section/header_responsive_web.dart
@@ -1,29 +1,27 @@
 import 'package:FDS_ASYA_PHILIPPINES/ui/screens/homepage/components/header_responsive_section/widgets.dart';
 import 'package:FDS_ASYA_PHILIPPINES/ui/screens/shared/utils/responsive.dart';
-import 'package:FDS_ASYA_PHILIPPINES/ui/screens/shared/values/colors.dart';
-import 'package:FDS_ASYA_PHILIPPINES/ui/screens/shared/values/images.dart';
 import 'package:FDS_ASYA_PHILIPPINES/ui/screens/shared/values/sizes.dart';
-import 'package:FDS_ASYA_PHILIPPINES/ui/screens/shared/values/strings.dart';
 import 'package:FDS_ASYA_PHILIPPINES/ui/screens/shared/widgets/content_area.dart';
-import 'package:FDS_ASYA_PHILIPPINES/ui/screens/shared/widgets/sizedbox.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-import '../../../shared/widgets/animation.dart';
+import '../../../shared/widgets/gradientcustomtext.dart';
+
 
 const double bodyTextSizeLg = 16.0;
 const double bodyTextSizeSm = 14.0;
 const double socialTextSizeLg = 18.0;
 const double socialTextSizeSm = 14.0;
 
+
+
 class HeaderResponsiveWeb extends StatefulWidget {
+  const HeaderResponsiveWeb({Key? key}) : super(key: key);
+
   @override
   _HeaderResponsiveWebState createState() => _HeaderResponsiveWebState();
 }
 
 class _HeaderResponsiveWebState extends State<HeaderResponsiveWeb> {
-  late Animation<double> _animation;
   @override
   void dispose() {
     super.dispose();
@@ -31,196 +29,98 @@ class _HeaderResponsiveWebState extends State<HeaderResponsiveWeb> {
 
   @override
   Widget build(BuildContext context) {
-    double headerIntroTextSize = responsiveSize(
-      context,
-      Sizes.TEXT_SIZE_24,
-      Sizes.TEXT_SIZE_48,
-      md: Sizes.TEXT_SIZE_36,
-    );
-    double screenWidth = widthOfScreen(context);
-    double sizeOfBlobSm = screenWidth * 0.3;
-    double sizeOfGoldenGlobe = screenWidth * 0.2;
-    double dottedGoldenGlobeOffset = sizeOfBlobSm * 0.4;
-    double heightOfBlobAndGlobe = computeHeight(dottedGoldenGlobeOffset, sizeOfGoldenGlobe, sizeOfBlobSm);
-    double heightOfStack = heightOfBlobAndGlobe * 2;
     return ContentArea(
-      child: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height:100),
-              Center(
-                child: Container(
-                  color: Colors.transparent,
-                  width: 1300,
-                  child: Image.asset(
-                    'assets/images/main-text-landing-page.png',
+      height: MediaQuery.of(context).size.height,
+      child: _buildDesktopHeader(context),
+    );
+  }
 
-                fit: BoxFit.contain,
-                  ),
-                //   child: Column(
-                //     children: [
-                //       AnimatedGradientText(
-                //         animation: _animation,
-                //         text: "Trusted Partner on your",
-                //       ),
-                //       AnimatedGradientText(
-                //         animation: _animation,
-                //         text: "Trusted Partner on your",
-                //       ),
-                //     ],
-                //   ),
+  Widget _buildDesktopHeader(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    final isMobile = screenWidth < 800;
 
+    final textAlign = isMobile ? TextAlign.center : TextAlign.justify;
+
+    final textItems = [
+      'Providing world class core banking software in the regional economy and community with short',
+      'Implementation time, tailor-made services, and redefining the holistic customer experiences.',
+    ];
+
+    return Stack(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 100),
+            Center(
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  double screenWidth = constraints.maxWidth;
+
+                  // Adjust this formula as needed for your layout
+                  double dynamicFontSize = (screenWidth * 0.08).clamp(50.0, 100.0);
+
+                  return GradientCustomText(
+                    text: 'Trusted partner on your Digitalization Journey.',
+                    style: TextStyle(
+                      fontSize: dynamicFontSize,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    gradient: linearGradient,
+                  );
+                },
+              ),
+            ),
+            isMobile ? SizedBox(height: 20) : SizedBox(height: 300),
+            Center(
+              child: Container(
+                // color: Colors.teal,
+                width: screenWidth * 0.9,
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Flex(
+                  direction: isMobile ? Axis.vertical : Axis.horizontal,
+                  mainAxisAlignment: isMobile ? MainAxisAlignment.center : MainAxisAlignment.spaceBetween,
+                  children: textItems.map((text) => textBox(text, textAlign)).toList(),
                 ),
               ),
-              SizedBox(height:20),
-              Center(
-                child: ClipRect(
-                  child: SizedBox(
-                    height: 150, // Half of the original circle height
-                    width: 300, // Optional: match image width
-                    child: Align(
-                      alignment: Alignment.topCenter, // or Alignment.bottomCenter
-                      child: Image.asset(
-                        'assets/images/dashed-circle.png',
-                        width: 300, // Full image width
-                        height: 300, // Full image height (needed so clipping works)
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 
-              SizedBox(height:40),
-              Stack(
-                children:[
-                  Padding(
-                    padding: const EdgeInsets.only(left: 130, top: 300),
-                    child: Container(
-                        width: 300,
-                        height: 300,
-                        child: Text('Providing world class core banking software in the regional economy and community with short',textAlign: TextAlign.justify,)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 1250, top: 300),
-                    child: Container(
-                        width: 300,
-                        height: 300,
-                        child: Text('Providing world class core banking software in the regional economy and community with short',textAlign: TextAlign.justify,)),
-                  ),
-                  Center(
-                    child: Container(
-                      child: Image.asset(
-                        'assets/images/phone-web.png',
-                        width: 1300,
-                        fit: BoxFit.contain,
-                        // height: 1000,
-                      ),
-                    ),
-                  ),
-                  // Center(
-                  //   child: Container(
-                  //     child: Image.asset(
-                  //       'assets/images/dashed-circle.png',
-                  //       width: 1300,
-                  //       fit: BoxFit.contain,
-                  //       // height: 1000,
-                  //     ),
-                  //   ),
-                  // ),
-
-
-                ] 
-              ),
-            ],
-          ),
-        ],
+  Widget textBox(String text, TextAlign textAlign) {
+    return Container(
+      margin: const EdgeInsets.all(20),
+      width: 300,
+      child: Text(
+        text,
+        textAlign: textAlign,
+        style: const TextStyle(fontSize: 16),
       ),
     );
   }
 }
+//   Widget buildTabletHeader() {
+//     return Column(
+//       children: [
+//         const SizedBox(height: 60),
+//         Image.asset('assets/images/main-text-landing-page.png', width: 600),
+//         const SizedBox(height: 30),
+//         Image.asset('assets/images/phone-web.png', width: 600),
+//         const Padding(
+//           padding: EdgeInsets.all(20.0),
+//           child: Text(
+//             'Banking software solutions for the regional economy and community.',
+//             textAlign: TextAlign.center,
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+//
+// }
 
+  // You can put these in separate files or in the same file.
 
-
-// Container(
-//   child: Image.asset(
-//     ImagePath.HOME_BACKGROUND,
-//   ),
-// ),
-// Container(
-//   margin: EdgeInsets.only(
-//     top: heightOfStack * 0.15,
-//     left: (sizeOfBlobSm * 0.35),
-//   ),
-//   child: Row(
-//     mainAxisSize: MainAxisSize.min,
-//     children: [
-//       // Column(
-//       //   crossAxisAlignment: CrossAxisAlignment.start,
-//       //   mainAxisSize: MainAxisSize.min,
-//       //   children: [
-//       //     ConstrainedBox(
-//       //       constraints: BoxConstraints(maxWidth: screenWidth),
-//       //       child: Text(
-//       //         StringConst.INTRO,
-//       //         textAlign: TextAlign.start,
-//       //         style: GoogleFonts.poppins(
-//       //           fontSize: headerIntroTextSize,
-//       //           fontWeight: FontWeight.w600,
-//       //           color: AppColors.white,
-//       //         ),
-//       //       ),
-//       //     ),
-//       //     ConstrainedBox(
-//       //       constraints: BoxConstraints(maxWidth: screenWidth),
-//       //       child: Text(
-//       //         StringConst.INTRO2,
-//       //         textAlign: TextAlign.start,
-//       //         style: GoogleFonts.poppins(
-//       //           fontSize: headerIntroTextSize,
-//       //           fontWeight: FontWeight.w600,
-//       //           color: AppColors.white,
-//       //         ),
-//       //       ),
-//       //     ),
-//       //     // ConstrainedBox(
-//       //     //   constraints: BoxConstraints(maxWidth: screenWidth),
-//       //     //   child: AnimatedTextKit(
-//       //     //     animatedTexts: [
-//       //     //       TypewriterAnimatedText(
-//       //     //         StringConst.INTRO3,
-//       //     //         textAlign: TextAlign.start,
-//       //     //         speed: Duration(milliseconds: 100),
-//       //     //         textStyle: GoogleFonts.poppins(
-//       //     //           fontSize: headerIntroTextSize,
-//       //     //           fontWeight: FontWeight.w600,
-//       //     //           color: AppColors.white,
-//       //     //         ),
-//       //     //       ),
-//       //     //     ],
-//       //     //     onTap: () {},
-//       //     //     isRepeatingAnimation: true,
-//       //     //     repeatForever: true,
-//       //     //   ),
-//       //     // ),
-//       //     SizedBoxH16(),
-//       //     ConstrainedBox(
-//       //       constraints: BoxConstraints(maxWidth: screenWidth * 0.40),
-//       //       child: SelectableText(
-//       //         StringConst.ABOUT_DEV,
-//       //         textAlign: TextAlign.justify,
-//       //         style: GoogleFonts.poppins(
-//       //           color: AppColors.white,
-//       //           fontWeight: FontWeight.w500,
-//       //           fontSize: 15,
-//       //           height: 1.7,
-//       //         ),
-//       //       ),
-//       //     ),
-//       //   ],
-//       // ),
-//     ],
-//   ),
-// ),
