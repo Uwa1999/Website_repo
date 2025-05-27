0ec297d0aab3091737edef9e57d8f72f2c1bdf75
jrmpcnd <jeremae.apacionado@cardmri.com>
2025-05-27 21:17:53 +0800

    responsive layout updated

--- lib/ui/screens/shared/utils/responsive.dart
+++ lib/ui/screens/shared/utils/responsive.dart
@@ -164,3 +164,38 @@ class Responsive extends StatelessWidget {
 // set the common device dimensions here for mobile / tablet / desktop
 
 const mobileWidth = 600;
+
+
+
+// utils/responsive_layout.dart
+
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobile;
+  final Widget tablet;
+  final Widget desktop;
+
+  const ResponsiveLayout({
+    Key? key,
+    required this.mobile,
+    required this.tablet,
+    required this.desktop,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    if (width >= 1024) {
+      return desktop;
+    } else if (width >= 600) {
+      return tablet;
+    } else {
+      return mobile;
+    }
+  }
+}
+
+
+
+
+
